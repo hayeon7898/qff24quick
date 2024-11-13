8c3f1c2d9bd7fc61eb90653a42fa79c7acecb4a8
hayeon7898 <hayeon22@korea.ac.kr>
2024-11-13 21:33:00 +0900

    fixed 400 error(/receive_data)

--- app/routes.py
+++ app/routes.py
@@ -133,7 +133,7 @@ def receive_data():
             }
         }
 
-        type_id, problem_number = parse_question(question)
+        type_id, problem_number = parse_question(question, db)
         add_score(username, type_id, problem_number, answer)
 
         # 성공적으로 데이터를 받았다면 JSON으로 응답
